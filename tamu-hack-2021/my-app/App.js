48b647d6430352eec9800a50312ceadace34ead8
Matias <none>
2021-01-30 16:12:37 -0600

    fix app please last time please i swear to god

--- tamu-hack-2021/my-app/App.js
+++ tamu-hack-2021/my-app/App.js
@@ -1,22 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Matias was here!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from "react";
+import AppLoading from 'expo-app-loading';
+import { Container, Text, Header, Content, Accordion } from "native-base";
+import { StatusBar } from "react-native";
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </Container>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
