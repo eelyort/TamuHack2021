c65d2370c99bf3505625c16340af0d6cb7ed0830
Matias <none>
2021-01-30 16:23:53 -0600

    working again

--- tamu-hack-2021/my-app/App.js
+++ tamu-hack-2021/my-app/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-import AppLoading from 'expo-app-loading';
 import { Container, Text, Header, Content, Accordion } from "native-base";
 import { StatusBar } from "react-native";
 export default class App extends React.Component {
   render() {
     return (
       <Container>
-        <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
+        <Text>Open up App.js to start working on your app!</Text>
       </Container>
     );
   }
